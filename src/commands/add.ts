0488ec128af7fe16b289a2dc5115d5c75ce05f73
Sungyu Kang <gron1gh1@gmail.com>
2024-09-15 00:53:56 +0900

    fix: check icon

--- src/commands/add.ts
+++ src/commands/add.ts
@@ -2,18 +2,31 @@ import { syncIcons } from "../utils/generateIcon";
 import { addIcons, checkIfConfigExists } from "../utils//config";
 import { generateBaseCode } from "../utils/generateBaseCode";
 import { init } from "./init";
+import { checkAvailableIcons } from "../utils/checkAvailableIcons";
+import { groupIconsByPrefix } from "../utils/groupIconsByPrefix";
+import { log } from "../utils/console";
 
 export const add = async (iconNames: string[]) => {
   try {
     if (!checkIfConfigExists()) {
       await init();
     }
+
+    const groupedIcons = groupIconsByPrefix(iconNames);
+    await Promise.all(
+      groupedIcons.map(async ([prefix, icons]) => {
+        if (!(await checkAvailableIcons(prefix, icons))) {
+          throw new Error(`Not found ${prefix}`);
+        }
+      })
+    );
+
     const config = await addIcons(iconNames);
     await generateBaseCode(config);
     await syncIcons(config);
   } catch (error) {
     if (error instanceof Error) {
-      console.error(error.message);
+      log.error(error.message);
     } else {
       console.error(error);
     }
