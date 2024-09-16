65e2a7efb3c2f5071a36c8b5d2bfe9eb479d4661
Sungyu Kang <gron1gh1@gmail.com>
2024-09-16 17:10:18 +0900

    fix: unused

--- src/utils/searchFunction.ts
+++ src/utils/searchFunction.ts
@@ -1,8 +1,3 @@
-const data = `
-function Fa1() {
-}
-`;
-
 export const searchFunction = (data: string, funcName: string): boolean => {
   const regex = new RegExp(`function\\s+${funcName}\\s*\\(`);
   return regex.test(data);
